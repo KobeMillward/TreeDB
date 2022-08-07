607c10edd1201f21937ea80bec4ecef62812e775
Kobe <jaikobework@gmail.com>
2022-08-07 17:24:50 +1000

    added tree tests

---
+++ TreeDB.Tests/TreeTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TreeDBNS;
+
+namespace TreeDBNS.Tests {
+    [TestClass]
+    public class TreeTest {
+        private TreeDB? db;
+        [TestInitialize]
+        public void BeforeEach() {
+            db = new TreeDB("DB");
+        }
+
+        [TestMethod]
+        public void TreeInit1() {
+            db.addTree("Tree");
+            Assert.AreEqual("Tree", db.getActiveName());
+        }
+
+        [TestMethod]
+        public void TreeInit2() {
+            db.addTree("Tree2");
+            Assert.AreEqual("Tree2", db.getActiveName());
+        }
+
+        [TestMethod]
+        public void TreeInit3() {
+            db.addTree("Tree");
+            db.addTree("Tree 2");
+            db.setActive("Tree 2");
+            Assert.AreEqual("Tree 2", db.getActiveName());
+        }
+    }
+}
