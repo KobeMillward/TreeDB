b3a1331374bf1259426137b3b2d41f427c96fbad
KobeMillward <jaikobework@gmail.com>
2022-08-13 17:20:55 +1000

    Updated node

--- TreeDB/INode.cs
+++ TreeDB/INode.cs
@@ -1,11 +1,22 @@
 ﻿namespace TreeDBNS {
     public interface INode {
+        // KEY
         public IComparable getKey();
+        
+        // DATA
         public Object? getData();
-        public bool setData();
+        public bool setData(Object? data);
+        
+        // Left Child
         public INode? getLChild();
-        public bool setLChild();
+        public bool setLChild(INode node);
+        public bool setLChild(IComparable key);
+        public bool setLChild(IComparable key, Object? data);
+
+        // Right Child
         public INode? getRChild();
         public bool setRChild(INode node);
+        public bool setRChild(IComparable key);
+        public bool setRChild(IComparable key, Object? data);
     }
 }
